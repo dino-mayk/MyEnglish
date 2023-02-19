8f7979b80a8b6c8debcb6c1446525d7f05336746
Nikita <nikitosikrasnov@gmail.com>
2023-02-19 12:16:41 +0300

    ability to add words, new design

--- MyEnglish/settings.py
+++ MyEnglish/settings.py
@@ -114,6 +114,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- words/views.py
+++ words/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from words.forms import AddWordForm
 from words.models import Word
 
 
@@ -9,4 +10,24 @@ def list(request):
     context = {
         'words': words,
     }
+    return render(request, template_name, context)
+
+
+def add(request):
+    form = AddWordForm(request.POST, request.FILES)
+    template_name = 'words/add.html'
+    context = {
+        'form': form,
+    }
+
+    if request.method == 'POST' and form.is_valid():
+        cleaned_data = form.cleaned_data
+        word = Word.objects.create(
+            user=request.user,
+            **cleaned_data
+        )
+        word.save()
+
+        return redirect('homepage:home')
+
     return render(request, template_name, context)
