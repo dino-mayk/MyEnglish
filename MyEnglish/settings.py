6a8dfc200be670dee539673d4890c01ca569553e
Nikita <nikitosikrasnov@gmail.com>
2023-03-05 22:02:42 +0300

    debug toolbar

--- MyEnglish/settings.py
+++ MyEnglish/settings.py
@@ -124,3 +124,24 @@ STATIC_ROOT = 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+if DEBUG:
+    MIDDLEWARE += [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ]
+    INSTALLED_APPS += [
+        'debug_toolbar',
+    ]
+    INTERNAL_IPS = [
+        '127.0.0.1',
+    ]
+
+    import mimetypes
+    mimetypes.add_type(
+        'application/javascript',
+        '.js',
+    )
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+    }
--- MyEnglish/urls.py
+++ MyEnglish/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -10,3 +12,14 @@ urlpatterns = [
 
     path('grappelli/', include('grappelli.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path(
+            '__debug__/',
+            include(
+                debug_toolbar.urls,
+            ),
+        ),
+    ]
